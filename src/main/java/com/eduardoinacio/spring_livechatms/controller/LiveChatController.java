e78ed96adf749e709b0dd3f7ebc989317a286c94
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-18 13:51:36 -0300

    Creating LiveChatController.

---
+++ src/main/java/com/eduardoinacio/spring_livechatms/controller/LiveChatController.java
@@ -0,0 +1,18 @@
+package com.eduardoinacio.spring_livechatms.controller;
+
+import com.eduardoinacio.spring_livechatms.domain.ChatInput;
+import com.eduardoinacio.spring_livechatms.domain.ChatOutput;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class LiveChatController {
+    @MessageMapping("/new-message")
+    @SendTo("/topic/livechat")
+    public ChatOutput newMessage(ChatInput chatInput) {
+        String message = HtmlUtils.htmlEscape(chatInput.username() + ": " + chatInput.message());
+        return new ChatOutput(message);
+    }
+}
