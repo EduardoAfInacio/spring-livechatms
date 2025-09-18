3a2afd382f77326c89934b8f1167f232a2c5b03a
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-18 12:43:16 -0300

    first commit

---
+++ src/main/java/com/eduardoinacio/spring_livechatms/SpringLivechatmsApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.spring_livechatms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLivechatmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLivechatmsApplication.class, args);
+	}
+
+}
